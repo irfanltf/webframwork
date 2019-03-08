1f6b10c068fd567b9e74b35bbc6637ea23363a2e
irfanltf <iluthfi469@gmail.com>
2019-03-08 11:29:13 -0800

    publish file blog

---
+++ blog/routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return '<h1>hello world</h1>
+  
+    ini adalah cara menampilkan hello world menggunakan framwork laravel di minggu pertama hehehehhehehe
+
+    ';
+});
